c9381879a9363fb4d8fe69f84cc53da30c641b7e
Александр Кунеков <fairyfromalfeya@gmail.com>
2022-09-18 21:32:44 +0200

    build: added cors origins

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-NestFactory.create(AppModule).then((app) =>
-  app.listen(process.env.PORT || 3000),
-);
+NestFactory.create(AppModule).then((app) => {
+  app.enableCors({
+    origin: (process.env.ALLOWED_ORIGINS || '').split(','),
+  });
+
+  return app.listen(process.env.PORT || 3000);
+});
